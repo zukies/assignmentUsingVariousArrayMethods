c4956ece3f1a7ce35bc3d088fffab51a269cc8ea
zukies <zukies28@gmail.com>
2022-08-28 16:46:37 +0200

    Adding files to main

--- variousArrayMethods.js
+++ variousArrayMethods.js
@@ -1,6 +1,4 @@
 "use strict";
-// const testData1 = [5, 2, 4, 1, 15, 8, 3];
-// const testData2 = [16, 6, 10, 5, 6, 1, 4];
 
 const dog = [
   {
@@ -24,113 +22,90 @@ const dog = [
     owners: ["Michael"],
   },
 ];
+
 // ARRAYS//
-const ownersEatTooMuch = [];
-const ownersEatTooLittle = [];
-const dogsEatRight = [];
-const arrayForAllDogsFood = [];
-const sortingArray = [];
 const ownerArray = [];
 const sortedArrays = [];
-const recommendedPortion = [];
-const twoArrays = [];
+const arrCurFood = [];
+const dogsEatExact = [];
 
-const copyOfDogsArray = dog.slice();
+// #1
+// ADDING THE EXTRA RECOMMENDED FOOD PROPERTY//
+
 dog.forEach(function (el) {
-  arrayForAllDogsFood.push(el.curFood);
-  ownerArray.push(el.owners);
+  el.recFood = Math.trunc(el.weight ** 0.75 * 28);
 });
 
-{
-  dog.forEach(function (el) {
-    arrayForAllDogsFood.push(el.curFood);
-  });
+// #2
+//FINDING SARAH'S DOG TO SEE IF THAT DOG IS EATING RIGHT
+const sarahDog = dog.find((el) => el.owners.includes("Sarah"));
+sarahDog < dog.recFood + dog.recFood * 0.1 &&
+sarahDog > dog.recFood - dog.recFood * 0.1
+  ? console.log(`Sarah's dog is eating right`)
+  : console.log(`Sarah's dog is not eating right`);
+
+// FORMULAE FOR TOO MUCH OR TOO LITTLE FOOD//
+
+// console.log(`${tooLittle}'s dogs are eating too much`);
+
+// #3 OWNERS OF DOGS THAT EAT TOO MUCH
+
+const eatTooMuch = dog
+  .filter((el) => el.curFood < el.recFood - el.recFood * 0.1)
+  .flatMap((el) => el.owners);
+console.log(eatTooMuch);
+console.log(`${eatTooMuch.join(" and ")}'s dogs are eating too much`);
+
+// #4
+// DOGS THAT EAT TOO LITTLE
+const EatTooLittle = dog
+  .filter((el) => el.curFood > el.recFood + el.recFood * 0.1)
+  .map((el) => el.owners)
+  .flat();
+console.log(EatTooLittle);
+console.log(`${EatTooLittle.join(" and ")}'s dogs are eating too little`);
+
+// #5
+// ARRAY OF DOGS EATING THE EXACT AMOUNT OF FOOD
+const eatExact = dog.filter((el) => el.curFood === el.recFood);
+
+eatExact.length === 0 ? console.log(true) : console.log(false);
+
+// #6
+// ANY DOGS EATING AN OK AMOUNT OF FOOD, IF TRUE..THEN THIS MEANS THERE ARE DOGS THAT EAT OK
+const dogsEatRight = dog
+  .filter(
+    (el) =>
+      el.curFood < el.recFood + el.recFood * 0.1 &&
+      el.curFood > el.recFood - el.recFood * 0.1
+  )
+  .map((el) => el.owners);
+
+// #7
+// ARRAY OF DOGS EATING THE RIGHT AMOUNT OF FOOD
+console.log(`${dogsEatRight}s dog is eating right`);
+
+if (dogsEatRight.length > 0) {
+  console.log(true);
 }
 
-// ARRAY SORTED
-const sortt = arrayForAllDogsFood.sort();
+//#8 ARRAY SORTED
+// const arrCurFood = [];
+const copyOfDogsArray = dog.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(copyOfDogsArray);
 
-sortt.forEach(function (value) {
+// OR...we can do the following as well...getting same result
+
+copyOfDogsArray.forEach(function (el) {
+  arrCurFood.push(el.curFood);
+  arrCurFood.sort();
+});
+
+arrCurFood.forEach(function (value) {
   copyOfDogsArray.forEach(function (el) {
     if (el.curFood === value) {
       sortedArrays.push(el);
     }
   });
 });
-
-// #8
 console.log(sortedArrays);
-
-// #1
-
-// ADDING THE EXTRA RECOMMENDED FOOD PROPERTY//
-dog.forEach(function (el) {
-  {
-    el.recommendedFood = () => (el.weight / 1000) * 0.75 * 28 * 1000;
-  }
-
-  // FORMULAE FOR TOO MUCH OR TOO LITTLE FOOD//
-
-  {
-    const tooMuchFood =
-      el.curFood + (el.recommendedFood() + el.recommendedFood() * 0.9);
-
-    const tooLittleFood =
-      el.curFood - (el.recommendedFood() + el.recommendedFood() * 1.1);
-
-    if (el.curFood < tooLittleFood) {
-      // #4
-      // DOGS THAT EAT TOO LITTLE
-      console.log(`${el.owners}'s dog eats too little`);
-      ownersEatTooLittle.push(el.owners);
-      // #3
-      // NAMES OF OWNERS WHO'S DOGS EAT TOO LITTLE
-      console.log(ownersEatTooLittle);
-    }
-    if (el.curFood > tooMuchFood) {
-      ownersEatTooLittle.push(el.curFood);
-
-      // #4 DOGS THAT EAT TOO MUCH
-      console.log(`${el.owners}'s dog eats too much`);
-      ownersEatTooMuch.push(el.owners);
-      // #3 NAMES OF OWNERS WHO'S DOGS EAT TOO LITTLE
-      console.log(ownersEatTooMuch);
-    }
-
-    // #5 THE RANGE (EATING THE RIGHT AMOUNT)
-    if (el.curFood < tooMuchFood && el.curFood > tooLittleFood) {
-      dogsEatRight.push(el.curFood);
-      console.log(`${el.owners}'s dog eats right`);
-    }
-
-    // #2
-    //FINDING SARAH'S DOG
-    {
-      if (el.owners.length > 1) {
-        const splitter = [];
-        splitter.push(el.owners);
-        const [spplit] = [...splitter];
-
-        spplit.forEach(function () {
-          spplit.find(function (sarahName) {
-            if (sarahName === "Sarah") {
-              el.curFood < tooMuchFood || el.curFood > tooLittleFood
-                ? console.log(`Sarah's dog is eating right`)
-                : console.log(`Sarah's dog is not eating right`);
-            }
-          });
-        });
-      }
-    }
-  }
-});
-
-// #6
-// IF THERE is A DOG THAT EATS THE RIGHT AMOUNT OF FOOD, IF TRUE..THEN THIS MEANS THERE ARE DOGS THAT EAT CORRECTLY
-if (dogsEatRight.length > 0) {
-  console.log(true);
-
-  // #7
-  // ARRAY OF DOGS EATING THE RIGHT AMOUNT OF FOOD
-  console.log(dogsEatRight);
-}
